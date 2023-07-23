05c7d520a0a97af967186ff6a7062851e6f806c0
Marian Minar <majamin@gmail.com>
2023-07-23 11:10:11 -0700

    update img paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <div className="mx-auto bg-light-gray py-[85px] desktop:py-[138px]">
         <div className="mx-auto h-[495px] w-[320px] rounded-2xl bg-white shadow-xl shadow-gray-300">
           <div className="flex h-full flex-col items-center p-4 text-center font-sans">
-            <img className="rounded-xl" src="/qr.png" alt="QR Code" />
+            <img className="rounded-xl" src="qr.png" alt="QR Code" />
             <div className="flex h-full flex-col items-center justify-center p-4 align-middle">
               <div className="mb-4 text-[20px] font-bold text-dark-blue">
                 Improve your front-end skills by building projects
