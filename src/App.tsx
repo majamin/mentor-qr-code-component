7f80cd0a14d43f682d4db895cbd941d8c6050578
Marian Minar <majamin@gmail.com>
2023-07-23 08:18:11 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-200 max-w-2xl mx-auto">
+        <div className="flex items-center">
+          <h1>
+            Marian Minar
+          </h1>
+          <div>
+            <ul>
+              <li>1614 Pope Pl.</li>
+              <li>Merritt B.C.</li>
+              <li>V1K 0B8</li>
+              <li>Canada</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
